1fa5bbb25a39e9428abe81976afbbad86d0ea530
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-18 21:22:14 +1000

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let stringArray = str.split('');
+    let revString = '';
+    for (const char of stringArray) {
+        revString = char + revString;
+    }
+    return revString;
 };
 
 // Do not edit below this line
