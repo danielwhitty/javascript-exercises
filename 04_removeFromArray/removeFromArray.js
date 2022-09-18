b848fe36a747655437f7e51ded5e7779c3d1f7d0
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-18 21:58:30 +1000

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        for (const element of array) {
+            if (arg === element) {
+                array.splice(array.indexOf(element), 1);
+                break;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
