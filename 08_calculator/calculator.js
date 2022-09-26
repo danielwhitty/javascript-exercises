41b094b251f364670dc0330e8f0816857eb55535
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-26 22:01:30 +1000

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
+}
+
+const subtract = (a, b) => {
+  return a - b;
 };
 
-const subtract = function() {
-	
+const sum = function(nums) {
+	const finalSum = nums.reduce(
+    (a, b) => a + b, 0
+  );
+  return finalSum;
 };
 
-const sum = function() {
-	
+const multiply = function(nums) {
+  const finalSum = nums.reduce(
+    (a, b) => a * b, 1
+    );
+    return finalSum;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (+num === 0 || +num === 1) {
+    return 1;
+  }
+  let finalSum = num;
+  while (num > 2) {
+    finalSum *= (num - 1);
+    num--;
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
