e59c6e6fa138de8b88e57e7eae92493a9c7efa14
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-27 17:43:37 +1000

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordArray = word.toLowerCase().split('');
+    // for (let char of wordArray) {
+    //     if (!(/[a-z]/).test(char)) {
+    //         wordArray.splice(wordArray.indexOf(char), 1);
+    //     }
+    // }
+    let i = 0;
+    while (i < wordArray.length) {
+        if ((/[a-z]/).test(wordArray[i])) {
+            i++;
+        } else {
+            wordArray.splice(i, 1);
+            continue;
+        }
+    }
 
+    for (let i = 0; i < wordArray.length - i; i++) {
+        if (wordArray[i] !== wordArray[wordArray.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
