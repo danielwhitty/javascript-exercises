86ba88940201c4df9759aa93ff2522c3fb04943d
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-27 18:10:52 +1000

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        return ((b.yearOfDeath || 2022) - b.yearOfBirth) - ((a.yearOfDeath || 2022) - a.yearOfBirth);
+    });
+    return people[0];
 };
 
 // Do not edit below this line
