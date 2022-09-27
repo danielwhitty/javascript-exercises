7fff5678dfecc189e8cdbe1691ad6eff44152270
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-27 17:53:12 +1000

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 1 || isNaN(+num)) {
+        return 'OOPS';
+    }
+    
+    const fibArray = [1, 1];
+    for (let i = 2; i < num; i++) {
+        fibArray.push(fibArray[0] + fibArray[1]);
+        fibArray.shift();
+    }
+    return fibArray[1];
 };
 
 // Do not edit below this line
